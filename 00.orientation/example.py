2b948eb9547e651a776f2ec26fcfcf8dc1871b08
矢代佑成 <yuseya2398rubiks@gmail.com>
2025-09-15 11:13:23 +0900

    課題完了

--- 00.orientation/example.py
+++ 00.orientation/example.py
@@ -1,1 +1,8 @@
-print
+zeinukikakaku=input("税抜き価格を入力してください")
+zeikomikakaku=int(zeinukikakakaku)*1.1
+if zeikomikakaku>=2000:
+    print("送料は無料です")
+else:
+    print("送料は350円です")
+    zeikomikakaku+=350
+    print("価格は",zeikomikakaku,"円です。")
